1b1d87fb92a5e56495851c4c9ae90e716a49bce8
gabriel farias ferreira <gabrielfariasferreira2008@gmail.com>
2026-07-21 13:45:46 -0300

    faet atualizando o arquivo app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,15 @@
 import { Component, importProvidersFrom, signal } from '@angular/core';
-//import { RouterOutlet } from '@angular/router';
-//import { Produto } from './components/produto/produto';
-import { ListaProdutos } from './features/Produtos/lista-produtos/lista-produtos';// import { ListaProdutos } from './components/produto/lista-produtos/lista-produtos';
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { ListaProdutos } from './features/Produtos/lista-produtos/lista-produtos';// 
+
 
 @Component({
   selector: 'app-root',
-  imports: [ListaProdutos],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('e-commerce');
+  nomeLoja = 'MagaLucia';//nome do e-commerce
 }
