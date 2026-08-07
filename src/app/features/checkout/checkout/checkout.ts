9c2f98e6007875e5d84846784a680b74c75fcb51
gabriel farias ferreira <gabrielfariasferreira2008@gmail.com>
2026-08-07 17:08:19 -0300

    feat modificando o produto e o checkout(com erro!)

--- src/app/features/checkout/checkout/checkout.ts
+++ src/app/features/checkout/checkout/checkout.ts
@@ -4,6 +4,9 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { FormGroup } from '@angular/forms';
 import { FormControl } from '@angular/forms';
 import { CarrinhoService } from '../../../core/services/carrinho.services';
+import { ValidationError } from '@angular/forms/signals';
+import { AbstractControl } from '@angular/forms';
+import { Validator } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
@@ -14,15 +17,31 @@ import { CarrinhoService } from '../../../core/services/carrinho.services';
 export class Checkout {
 carrinho = inject (CarrinhoService);
 formulario = new FormGroup({
- nome: new FormControl(''),
- email: new FormControl(''),
+ nome: new FormControl(',[Validators.required Validators.minlength()]'),
+ email: new FormControl('[validators.required Validators.email]'),
  endereco: new FormControl (''),
 
 });
 finalizar (){
+  if(this.Formulario.invalido){
+    console.log('Formulario Invalido');
+    return
+  }
+      const dados = this.Formulario.value;
+      const dados = this.carrinhoService.itens();
+
+
+
     console.log('Dados da Formulário: ',this.formulario.value);
    console.log('itens do carrinho; ',this.carrinho.quantidade());
 }
 
 }
-
+function nomeSemNumeros(control; AbstractControl): ValidationError |null{
+const valor = control.value;
+if (valor neturn null;)
+if(/id.test(valor)){
+  return [numeroInvalido: true]
+}
+return null;
+}
