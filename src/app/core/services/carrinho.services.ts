184849e74925b6f5fc4be1b9d50781f624d76a9b
gabriel farias ferreira <gabrielfariasferreira2008@gmail.com>
2026-08-10 17:08:38 -0300

    modificando o carrinho services

--- src/app/core/services/carrinho.services.ts
+++ src/app/core/services/carrinho.services.ts
@@ -2,20 +2,27 @@ import{Injectable} from '@angular/core';
 import{signal} from '@angular/core';
 import{computed} from '@angular/core';
 
+type ItemCarrinho = {
+    nome: string;
+    preco: number;
+
+}
 @Injectable({
  providedIn: 'root'
 })
 export class CarrinhoService {
  
-    private carrinho = signal<{nome: string, preco: number}[]>([]);
+    private carrinho = signal<ItemCarrinho[]>([]);
     //?seletores
     item=computed(() => this.carrinho());
     quantidade=computed(() => this.carrinho().length);
     total=computed(() =>
-         this.carrinho().reduce((total, item) => total + item.preco, 0)
+         this.carrinho().reduce((total, item) =>
+            total + item.preco, 0)
 );
+carrinhoVazio = computed(() => this.carrinho().length === 0); 
 //TODO: Ações
-adicionarItem(produto: {nome: string, preco: number}) {
+adicionarItem(produto: ItemCarrinho) {
     this.carrinho.update(lista=> [...lista, produto]);
 }  
 limparCarrinho() {    
